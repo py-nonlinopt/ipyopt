88f11cef5dce3b014fdb4675140b522ee1236546
Gerhard Bräunlich <g.braeunlich@disroot.org>
2020-12-18 13:37:30 +0100

    remove tp_print in IPyOptProblemType (no longer valid in python 3.9)

--- src/ipyopt_module.c
+++ src/ipyopt_module.c
@@ -373,7 +373,6 @@ PyTypeObject IPyOptProblemType =
    .tp_basicsize = sizeof(IPyOptProblemObject),
    .tp_itemsize = 0,
    .tp_dealloc = (destructor)py_ipopt_problem_dealloc,
-   .tp_print = 0,
    .tp_getattr = 0,
    .tp_setattr = 0,
    .tp_as_async = 0,
